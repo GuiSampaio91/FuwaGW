3980f9379f1514c53063654eaac9e8b263534fc7
GuiSampaio91 <143126300+GuiSampaio91@users.noreply.github.com>
2025-08-30 03:33:34 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const SHEET_ID = "1TZnHFtjc3iPwNZO2BDObHxuVJC7c6-ykIUQ6SJFJ0xM"; // o ID é o trecho entre /d/ e /edit na URL
+export const API_KEY = "AIzaSyBoM7ovDyexRYNfCWGuDRtCFN0_wgzuCWk";
+
+export const SHEET_NAME = "Guild Wars";
+
+export const RANGE_A1 = `'${SHEET_NAME}'!A1:Q1000`;

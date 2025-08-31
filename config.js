94689cb14de3f82479961604c48cafa8246eeee7
GuiSampaio91 <143126300+GuiSampaio91@users.noreply.github.com>
2025-08-31 01:48:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,8 @@ export const API_KEY = "AIzaSyBoM7ovDyexRYNfCWGuDRtCFN0_wgzuCWk";
 export const SHEET_NAME = "Guild Wars";
 
 export const RANGE_A1 = `'${SHEET_NAME}'!A1:Q1000`;
+
+export const APPS_SCRIPT = {
+  BASE_URL: 'https://script.google.com/macros/s/AKfycbxWvzUqKejR9F4Ty8_QR984n9JYLTMwQpr9VAtfEF8HsV8qiQN4weuAeN7gPzTg2Ft7og/exec',
+  CLIENT_TOKEN: 'fwf-client-v1'
+};
